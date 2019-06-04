9b564f73d0731c4fb41d3a33a3b29c245593c69b
solatticus <37618929+solatticus@users.noreply.github.com>
2019-06-04 18:52:12 -0400

    universal sql connection string would be nice

--- src/Genesis/Generation/OutputTaskResult.cs
+++ src/Genesis/Generation/OutputTaskResult.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Genesis.Generation
 {
-    public class OutputTaskResult : BlankTaskResult, ITaskResult
+    public class OutputTaskResult : BlankTaskResult
     {
         public override bool Success { get; set; } = false;
         public override string Message { get; set; } = "Fail";
