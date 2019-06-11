93d5be41b296b9fa133c5f6620dfcde0408f3760
solatticus <37618929+solatticus@users.noreply.github.com>
2019-06-10 20:53:47 -0400

    removed obsolete commands

--- src/Genesis.Cli/Commands/GoCommand.cs
+++ src/Genesis.Cli/Commands/GoCommand.cs
@@ -1,23 +1,23 @@
-﻿using Genesis.Cli.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Genesis.Cli.Extensions;
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Genesis.Cli.Commands
-{
-    public class GoCommand : GenesisCommand
-    {
-        public override string Name { get => "go"; }
+//namespace Genesis.Cli.Commands
+//{
+//    public class GoCommand : GenesisCommand
+//    {
+//        public override string Name { get => "go"; }
 
-        public override string Description => "Execute the current configuration.";
+//        public override string Description => "Execute the current configuration.";
 
-        public override async Task<ITaskResult> Execute(GenesisContext genesis, string[] args)
-        {
-            genesis.ValidateGenesisContext();
+//        public override async Task<ITaskResult> Execute(GenesisContext genesis, string[] args)
+//        {
+//            genesis.ValidateGenesisContext();
 
-            return await genesis.ExecuteConfiguration();
-        }
-    }
-}
+//            return await genesis.ExecuteConfiguration();
+//        }
+//    }
+//}
