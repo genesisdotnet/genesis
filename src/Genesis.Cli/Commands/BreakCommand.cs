6e32423380777f3a2ea284d2dc560510213664c4
solatticus <37618929+solatticus@users.noreply.github.com>
2019-06-17 19:38:31 -0400

    output

--- src/Genesis.Cli/Commands/BreakCommand.cs
+++ src/Genesis.Cli/Commands/BreakCommand.cs
@@ -17,6 +17,10 @@ namespace Genesis.Cli.Commands
             //NOTE: This apparently can't affect anything on the Program class itself. (without some voodoo)
             //So, just dump some text and check for this command in Program. 
             Text.DarkYellowLine("Breaking...");
+            Text.Yellow($"Dumping to a prompt, [");
+            Text.CliCommand(GetType().Name.Replace("Command", ""));
+            Text.YellowLine($"] was called.");
+            Text.Line();
 
             return await Task.FromResult(new BlankTaskResult());
         }
