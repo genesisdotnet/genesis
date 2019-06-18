dc714693c34e0212eb93fd4a7bdf49ea3b9eac18
solatticus <37618929+solatticus@users.noreply.github.com>
2019-06-17 20:06:20 -0400

    added Excel source shell

---
+++ src/Executors/Input/Genesis.Input.Excel/ExcelSheetConfig.cs
@@ -0,0 +1,12 @@
+﻿using Genesis.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genesis.Input.Excel
+{
+    public class ExcelSheetConfig : InputConfiguration
+    {
+        public string SourcePath { get; set; } = string.Empty;
+    }
+}
