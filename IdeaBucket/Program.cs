0cb74cce1b51649b6eda0bc57fef9d32b6ed45df
Matthew Whited <matt.whited@lightwellinc.com>
2020-01-11 11:20:33 -0500

    wtf

---
+++ IdeaBucket/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IdeaBucket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = from f in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.KeyDef")
+                        select (f, Parse(filePath: f));
+
+            var values = files.ToArray();
+        }
+
+        private static IEnumerable<(string, string)> Parse(string filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string key = null;
+                StringBuilder value = null;
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine()?.Split(new[] { '|' }, 2).FirstOrDefault()?.Trim();
+
+                    if (line.StartsWith(':') && line.EndsWith(':'))
+                    {
+                        if (key != null)
+                        {
+                            yield return (key, value?.ToString());
+                            value = null;
+                            key = null;
+                        }
+                        key = line.Trim(':');
+                    }
+                    else if (string.IsNullOrWhiteSpace(line))
+                    {
+                        //Ignore me
+                    }
+                    else
+                    {
+                        (value ?? (value = new StringBuilder())).Append(line).AppendLine();
+                    }
+                }
+
+                if (key != null)
+                {
+                    yield return (key, value?.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ src/Genesis/Suggestions/ReplacementProvider.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Genesis.Suggestions
+{
+    /*
+    public interface IGenesisExecutor<TGenesisExecutionResult> where TGenesisExecutionResult : IGenesisExecutionResult
+    {
+        string CommandText { get; }
+        string Description { get; }
+        string FriendlyName { get; }
+
+        bool Initialized { get; }
+        Task Initialize();
+        Task DisplayConfiguration();
+        Task<bool> EditConfig<TPropertyType>(string propertyName, TPropertyType value);
+        Task<TGenesisExecutionResult> Execute(GenesisContext genesis, string[] args);
+    }
+    */
+
+    /*
+    Definition =>
+        Entities
+            Person
+                Name
+                Id
+            User
+                UserName
+                Email
+            Contact
+                Address1
+                Address2
+
+    Model =>
+        Object
+            Person
+                Properties
+
+    Description =>
+        Object 
+            Keys
+                Objectname
+                Propert1
+                Propt[]
+
+    Exection
+        I!MyType!
+        !MtYpe
+
+
+
+     * 
+     Scoping
+        key, value
+        key, value
+     */
+
+    public interface IReplacementPaser
+    {
+        IEnumerable<(string key, Func<object, string> execFunc)> Parse(string template);
+    }
+
+    public abstract class ReplacementPaserBase<T> : IReplacementPaser
+    {
+        public abstract IEnumerable<(string key, Func<T, string> execFunc)> Parse(string template);
+
+        IEnumerable<(string key, Func<object, string> execFunc)> IReplacementPaser.Parse(string template)
+        {
+            return from e in Parse(template)
+                   let k = e.key
+                   let f = new Func<object, string>(o => o is T t ? e.execFunc(t) : null)
+                   select (k, f);
+        }
+    }
+    public class ReplacementProvider<T>
+    {
+        private readonly IEnumerable<(string key, Func<T, string> execFunc)> _replacements;
+
+        public ReplacementProvider(IEnumerable<KeyValuePair<string, Func<T, string>>> replacements)
+            : this(replacements.Select(kvp => (kvp.Key, kvp.Value)))
+        {
+        }
+        public ReplacementProvider(IEnumerable<(string key, Func<T, string> execFunc)> replacements)
+        {
+            _replacements = replacements;
+        }
+        public ReplacementProvider(params (string key, Func<T, string> execFunc)[] replacements)
+            : this(replacements.AsEnumerable())
+        {
+        }
+
+        public string DoShit(T input, string template)
+        {
+            var sb = new StringBuilder(template);
+
+            foreach (var r in _replacements)
+            {
+                sb.Replace(r.key, r.execFunc(input));
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class ReplacementProviderFactory
+    {
+        public ReplacementProvider<T> Get<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
