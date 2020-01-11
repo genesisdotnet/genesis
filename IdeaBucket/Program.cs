d5cc453cde8ffcf6a3d648d632000a0c76af05dd
Matthew Whited <matt.whited@lightwellinc.com>
2020-01-11 12:18:43 -0500

    update my crazy

--- IdeaBucket/Program.cs
+++ IdeaBucket/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,13 +12,53 @@ namespace IdeaBucket
     {
         static void Main(string[] args)
         {
-            var files = from f in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.KeyDef")
-                        select (f, Parse(filePath: f));
+            var codes = from f in Directory.EnumerateFiles(Environment.CurrentDirectory, "*.KeyDef")
+                        from s in f.GetKeysAndScripts()
+                        select (
+                        type: Path.GetFileNameWithoutExtension(f),
+                        key: s.key,
+                        script: s.script,
+                        operation: s.script.ToFunc()
+                        );
 
-            var values = files.ToArray();
+            var values = codes.ToArray();
         }
 
-        private static IEnumerable<(string, string)> Parse(string filePath)
+    }
+    public static class ToolsEx
+    {
+        private class Inputs
+        {
+            public dynamic input { get; internal set; }
+        }
+        public static Func<dynamic, string> ToFunc(this string script)
+        {
+            // https://github.com/dotnet/roslyn/wiki/Scripting-API-Samples
+            var lines = script.ToLines();
+            var code = string.Join(";" + Environment.NewLine, lines);
+
+            var csScript = CSharpScript.Create<string>(
+                code,
+                globalsType: typeof(Inputs)
+                );
+
+            var csDelegate = csScript.CreateDelegate();
+
+           // csDelegate.run
+
+
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<string> ToLines(this string text)
+        {
+            using var reader = new StringReader(text);
+            string line = null;
+            while ((line = reader.ReadLine()) != null)
+                yield return line;
+        }
+
+        public static IEnumerable<(string key, string script)> GetKeysAndScripts(this string filePath)
         {
             using (var reader = new StreamReader(filePath))
             {
@@ -43,7 +85,8 @@ namespace IdeaBucket
                     }
                     else
                     {
-                        (value ?? (value = new StringBuilder())).Append(line).AppendLine();
+                        if (value != null) value.AppendLine();
+                        (value ?? (value = new StringBuilder())).Append(line);
                     }
                 }
 
