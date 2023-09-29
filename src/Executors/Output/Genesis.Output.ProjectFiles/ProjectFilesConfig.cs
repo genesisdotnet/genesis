e8a031067afdeecdae1e574b40c56ba258e0d04a
Solatticus <solatticus@pm.me>
2023-09-29 09:03:19 -0400

    added .csproj generator (janky w/o project refs)

---
+++ src/Executors/Output/Genesis.Output.ProjectFiles/ProjectFilesConfig.cs
@@ -0,0 +1,15 @@
+﻿using Genesis.Output;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Genesis.Output.Poco
+{
+    public class ProjectFilesConfig : GeneratorConfiguration
+    {
+        public Dictionary<string, string> Projects { get; set; } = new();
+        public string Language { get; set; } = "C#";
+        public string Overwrite { get; set; } = "all";
+        public string Preserve { get; set; } = "none";
+    }
+}
---
+++ src/Executors/Output/Genesis.Output.ProjectFiles/ProjectFilesGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Genesis.Cli;
+using Genesis.Output;
+
+#nullable enable 
+
+namespace Genesis.Output.Poco
+{
+    public class ProjectFilesGenerator : OutputExecutor
+    {
+        public override string CommandText => "project-files";
+        public override string Description => "Generates a project file for each entry in the .gen file";
+        public override string FriendlyName => "Project File generator";
+
+        public ProjectFilesConfig Config { get; set; } = new ProjectFilesConfig();
+
+        protected override void OnInitialized()
+        {
+            Config = (ProjectFilesConfig)Configuration;
+
+            Actions.Add(Tokens.Namespace, (ctx, obj) => Config.Namespace);
+        }
+
+        public override async Task<IGenesisExecutionResult> Execute(GenesisContext genesis, string[] args)
+        {
+            var output = Template.Raw;
+
+            foreach (var proj in Config.Projects)
+            {
+                var outPath = Path.Combine(proj.Value, proj.Key);
+
+                await File.WriteAllTextAsync(outPath.Replace('<', '_').Replace('>', '_'), output); //hacky, can't save fileNames with '<' or '>' in the name
+
+                Text.White($"Wrote '"); Text.Yellow(outPath); Text.WhiteLine("'");
+            }
+
+            var result = new OutputGenesisExecutionResult
+            {
+                Success = true
+            };
+
+            return result;
+        }
+
+        protected override GenesisDependency OnBeforeWriteDependency(object sender, DependencyEventArgs e)
+        {
+            return new GenesisDependency("","",""); //for now
+        }
+
+        public async Task ExecuteGraph(ObjectGraph objectGraph)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
